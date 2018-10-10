defb000748c6cf3ea72c6bf7ce445ff96161741b
arapl3y <rapley3@gmail.com>
2018-10-10 16:22:07 +1100

    Remove unused import

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import serverless from "serverless-http";
 import graphiql from "graphql-playground-middleware-express";
 import merge from "lodash.merge";
 import { ApolloServer } from "apollo-server-express";
-import { makeExecutableSchema } from "graphql-tools";
 import { todoListResolvers, todosResolvers } from "./resolvers";
 import { todoListTypeDefs, todosTypeDefs } from "./types";
 import * as AWS from "aws-sdk";
