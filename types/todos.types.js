d976d4ea7df9c7deec1e3d0cb37b55f6add751ad
arapl3y <rapley3@gmail.com>
2018-10-10 22:20:50 +1100

    Add remaining todos types

--- types/todos.types.js
+++ types/todos.types.js
@@ -5,5 +5,32 @@ export const todosTypeDefs = gql`
     id: ID!
     text: String!
     completed: Boolean!
+    listId: ID!
+  }
+
+  input UpdatedTodo {
+    id: ID!
+    text: String
+    completed: Boolean
+    listId: ID
+  }
+
+  input NewTodo {
+    id: ID
+    text: String!
+    completed: Boolean
+    listId: ID!
+  }
+
+  extend type Query {
+    allTodos: [Todo]!
+    Todo(id: ID!): Todo!
+    getTodosByTodoList(listId: ID!): [Todo]!
+  }
+
+  extend type Mutation {
+    newTodo(input: NewTodo!): Todo!
+    removeTodo(id: ID!): Todo!
+    updateTodo(input: UpdatedTodo!): Todo!
   }
 `;
